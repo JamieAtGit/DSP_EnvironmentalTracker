aa2fd5e19241c1d8c762577b9e269f17f9cf5299
JamieAtGit <jamie3.young@live.uwe.ac.uk>
2025-05-13 17:02:41 +0100

    Ready for deployment

--- backend/api/app.py
+++ backend/api/app.py
@@ -642,7 +642,13 @@ def test():
 
 
 
+#if __name__ == "__main__":
+ #   print("🚀 Flask is launching...")
+  #  app.run(debug=True)
+   # host="0.0.0.0", port=5000,
+   
 if __name__ == "__main__":
-    print("🚀 Flask is launching...")
-    app.run(debug=True)
-    #host="0.0.0.0", port=5000, 
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+ 
