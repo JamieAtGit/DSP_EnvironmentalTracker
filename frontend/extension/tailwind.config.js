5d151f76619d3c7e4762d9e7cad783e9b5091ade
JamieAtGit <jamie3.young@live.uwe.ac.uk>
2025-05-14 13:34:06 +0100

    Fix: add tailwind and postcss config for Netlify

---
+++ frontend/extension/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./popup-app/**/*.{js,jsx,ts,tsx,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
